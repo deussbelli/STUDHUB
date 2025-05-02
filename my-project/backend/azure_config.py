89011db82480221f366113734a4bf3bf231d2cee
deussbelli <sitis.belli@gmail.com>
2025-05-02 08:21:04 +0300

    add files

--- my-project/backend/azure_config.py
+++ my-project/backend/azure_config.py
@@ -4,10 +4,10 @@ import msal
 import os
 
 # Налаштування для Azure AD (можна задати через змінні середовища або прямо тут)
-AZURE_CLIENT_ID = os.environ.get("AZURE_CLIENT_ID", "f863a945-4655-4b9c-915a-df4efa0139e0")
-AZURE_CLIENT_SECRET = os.environ.get("AZURE_CLIENT_SECRET", "Rfe8Q~QLwd6P16csd6BdhBjtm83G2wXLa2vrTbex")
-AZURE_TENANT_ID = os.environ.get("AZURE_TENANT_ID", "5065d102-3127-4ad5-b4cd-331de41ef39c")  # або GUID, або домен типу studhub.onmicrosoft.com
-AZURE_REDIRECT_URI = os.environ.get("AZURE_REDIRECT_URI", "http://localhost:5000/auth/azure/callback")
+AZURE_CLIENT_ID = os.environ.get("AZURE_CLIENT_ID")
+AZURE_CLIENT_SECRET = os.environ.get("AZURE_CLIENT_SECRET")
+AZURE_TENANT_ID = os.environ.get("AZURE_TENANT_ID")  # або GUID, або домен типу studhub.onmicrosoft.com
+AZURE_REDIRECT_URI = os.environ.get("AZURE_REDIRECT_URI")
 
 # Authority URL (важливо: має бути валідний TENANT_ID!)
 AZURE_AUTHORITY = f"https://login.microsoftonline.com/{AZURE_TENANT_ID}"
